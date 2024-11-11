150d0c92a083f9d36d958635037bae44f6e52840
Phạm Việt <brogun2002@gmail.com>
2024-11-11 19:23:15 +0700

    add basic_operator.py and and calculate basic operator

---
+++ basic_operator.py
@@ -0,0 +1,26 @@
+# Basic operator 
+
+number_1 = int(input())
+number_2 = int(input())
+
+# Sum 
+sum = number_1 + number_2
+
+# Minus 
+minus = number_1 - number_2
+
+# Multiply
+multiply = number_1 * number_2
+
+# Division 
+division = number_1 / number_2
+
+# Power 
+power = number_1 ** number_2
+
+
+print("Sum = ", sum)
+print("Minus = ", minus)
+print("Multiply = ", multiply)
+print("Division = ", division)
+print("Power = ", power)
