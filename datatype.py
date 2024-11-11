1e88caed260b1d49a8477c04cacd2292bba5487d
Phạm Việt <brogun2002@gmail.com>
2024-11-11 18:19:30 +0700

    Add data type and how to initialize variable

---
+++ datatype.py
@@ -0,0 +1,17 @@
+# Initialize variables
+
+# Integer var
+integer_var = 10
+print(integer_var)
+
+# Float var
+float_var = 3.2
+print(float_var)
+
+# String var
+string_var = "Pham Quoc Viet"
+print(string_var)
+
+# Boolean var
+boolean_var = True
+print(boolean_var)
