c2dcd09cf300184318757bb324f227a220a2c673
Phạm Việt <brogun2002@gmail.com>
2024-11-11 18:10:05 +0700

    print operator

--- print.py
+++ print.py
@@ -1,6 +1,12 @@
+# Print and show in terminal
 print("Hello Everyone")
 
 print("My name is Viet")
 
 print("This is my program")
 
+# Calculate operator and print
+print(3200 + 1000)
+print(3200 - 1000)
+print(3200 * 1000)
+print(3200 / 1000)
