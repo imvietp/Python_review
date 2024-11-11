4309513091f6fe8e183fcffb91e96b9fb93fbe68
Phạm Việt <brogun2002@gmail.com>
2024-11-11 19:01:19 +0700

    add Input 2 numbers and calculate operators

--- input_output.py
+++ input_output.py
@@ -12,3 +12,15 @@ print("Type your age: ")
 age = int(input())
 
 print("My name is " + name + ". This month I will be " + str(age))
+
+
+# Input 2 numbers and calculate operators
+print("Number 1 = ") 
+number_1 = int(input())
+print("Number 2 = ") 
+number_2 = int(input())
+
+print("A + B = " + str(number_1 + number_2))
+print("A - B = " + str(number_1 - number_2))
+print("A * B = " + str(number_1 * number_2))
+print("A / B = " + str(number_1 / number_2))
