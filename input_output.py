6a5f677f28372c23113738947d1b9e62d04c1998
Phạm Việt <brogun2002@gmail.com>
2024-11-11 18:57:03 +0700

    add Type my name my age and hometown

--- input_output.py
+++ input_output.py
@@ -2,3 +2,13 @@
 print("Your name is ")
 name = input()
 print("My name is " + name)
+
+# Type my name my age and hometown
+
+print("Type your name: ") 
+name = input()
+
+print("Type your age: ") 
+age = int(input())
+
+print("My name is " + name + ". This month I will be " + str(age))
