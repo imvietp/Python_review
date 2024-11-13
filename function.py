86d70a264c10708cc7deb222a926242cf0dacca9
Phạm Việt <brogun2002@gmail.com>
2024-11-14 01:53:57 +0700

    add Print the largest element among 3 elements

--- function.py
+++ function.py
@@ -1,5 +1,5 @@
 # Calculate sum of element in array
-
+'''
 def sum(array):
     total = 0
     for i in array:
@@ -10,3 +10,18 @@ def sum(array):
 
 lst = [1, 3, 5]
 print(sum(lst))
+'''
+
+# Print the largest element among 3 elements
+def largest_element (a, b, c):
+    if a > b and a > c:
+        return a
+    elif b > a and b > c:
+        return b
+    else:
+        return c
+    
+a = int(input())
+b = int(input())
+c = int(input())
+print(largest_element(a, b, c))
